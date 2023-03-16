24793ea336aded02bf8487a3335895a3f15b00d5
天翔我梦 <zrhdyx86426@163.com>
2023-03-16 10:12:06 +0800

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="这是我的简历项目"/>
   </div>
 </template>
 
